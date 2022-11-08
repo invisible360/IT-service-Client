0a8f196539e3641407ed997c64102040b27a4e25
Zakir Hossain <zakirh817@gmail.com>
2022-11-09 00:29:48 +0600

    auth context, firebase added

---
+++ src/context/AuthContext/AuthProvider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { getAuth } from 'firebase/auth'
+import { createContext } from 'react';
+import app from './firebase/firebase.config';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+
+    const authinfo = {
+
+    }
+
+    return (
+        <AuthContext.Provider value={authinfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
