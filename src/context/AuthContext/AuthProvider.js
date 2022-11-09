1350ab204a0338af948583c93f444102e6ab2f25
Zakir Hossain <zakirh817@gmail.com>
2022-11-09 22:39:09 +0600

    authentication system implemented in context

--- src/context/AuthContext/AuthProvider.js
+++ src/context/AuthContext/AuthProvider.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { getAuth } from 'firebase/auth'
+import React, { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { createContext } from 'react';
 import app from '../../firebase/firebase.config';
 
@@ -7,7 +7,8 @@ export const AuthContext = createContext();
 
 const auth = getAuth(app)
 
-const AuthProvider = ({ children }) => {const [user, setUser] = useState(null);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
@@ -43,7 +44,7 @@ const AuthProvider = ({ children }) => {const [user, setUser] = useState(null);
     }
 
     return (
-        <AuthContext.Provider value={authinfo}>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
