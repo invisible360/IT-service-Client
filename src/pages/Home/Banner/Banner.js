fcd5d1b0ec5398c7f4d496a7d2ef903d4c008545
Zakir Hossain <zakirh817@gmail.com>
2022-11-10 02:34:56 +0600

    tailwind and flowbit added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Carousel } from 'flowbite-react';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Here A Banner Will Be added</h1>
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel>
+                <img
+                    src="https://infobellstech.com/images/resource/itsolution.jpg"
+                    alt="..."
+                />
+                <img
+                    src="https://qph.cf2.quoracdn.net/main-qimg-fea75ed7f9c05efb30bc02ea7927ae79"
+                    alt="..."
+                />
+                <img
+                    src="https://media.istockphoto.com/id/531861190/photo/missing-puzzle-piece-problem-and-solution-white.jpg?s=612x612&w=0&k=20&c=hU0KmkOHgvVtF5H0ILcv18kDpV7_T0CIeGudJAL_MEg="
+                    alt="..."
+                />
+                <img
+                    src="https://hexaware.com/wp-content/uploads/2019/10/sap-disaster-recovery-solution-implementation-on-azure-for-a-leading-multi-technological-solutions-provider.jpg"
+                    alt="..."
+                />
+                <img
+                    src="https://ditech.co/uploads/images/services/1646203183-59727757.jpg?pq=low"
+                    alt="..."
+                />
+            </Carousel>
         </div>
     );
 };
--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext/AuthProvider';
+import { Navbar } from 'flowbite-react';
 
 const Header = () => {
     const { user, logout } = useContext(AuthContext);
@@ -13,7 +14,7 @@ const Header = () => {
 
     return (
         <div>
-            <Link to='/'>Home</Link>
+            {/* <Link to='/'>Home</Link>
             {
                 user && user.uid ?
                     <>
@@ -24,7 +25,57 @@ const Header = () => {
                     :
                     <Link to='/login'>Login</Link>
             }
-            <Link to='/blogs'>Blog</Link>
+            <Link to='/blogs'>Blog</Link> */}
+
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand>
+                    <span className="self-center whitespace-nowrap text-2xl font-bold dark:text-white">
+                        <Link to='/'>IT-Solution</Link>
+                    </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Navbar.Toggle />
+                </div>
+
+                <Navbar.Collapse>
+                    <Navbar.Link active={true}>
+                        <Link to='/'>Home</Link>
+                    </Navbar.Link>
+
+                    {
+                        (user && user.uid) &&
+                        <>
+                            <Navbar.Link >
+                                <Link to='/myreviews'>My Reviews</Link>
+                            </Navbar.Link>
+
+                            <Navbar.Link >
+                                <Link to='/addservice'>Add Service</Link>
+                            </Navbar.Link>
+                        </>
+
+                    }
+
+                    <Navbar.Link >
+                        <Link to='/blogs'>Blog</Link>
+                    </Navbar.Link>
+
+                    {
+                        user && user.uid ?
+                            <Navbar.Link >
+                                <button onClick={handleLogout}>Log Out</button>
+                            </Navbar.Link>
+                            :
+                            <Navbar.Link >
+                                <Link to='/login'>Login</Link>
+                            </Navbar.Link>
+
+                    }
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
